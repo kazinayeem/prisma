231f330bc234b0b0ac75464a1b2a226b75a812da
kazinayeem <mohammadalinayeem360@gmail.com>
2025-01-29 17:25:57 +0600

    update code and fix problem

--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -1,22 +1,33 @@
+import { useEffect, useState } from "react";
 import Link from "next/link";
 import { getTodos } from "../actions";
 
-export default async function page() {
-  const todos = await getTodos();
+export default function Page() {
+  const [todos, setTodos] = useState<
+    { id: string; title: string; completed: boolean }[]
+  >([]);
+
+  // Fetch todos when the component mounts
+  useEffect(() => {
+    const fetchTodos = async () => {
+      const data = await getTodos();
+      setTodos(data);
+    };
+
+    fetchTodos();
+  }, []); // Empty dependency array ensures this runs only once on component mount
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
-      {/* show todos  */}
       <div className="w-full max-w-md p-8 bg-white rounded-lg shadow-md">
         <h1 className="text-2xl font-semibold text-center text-gray-800 mb-6">
           Task List
         </h1>
-        <ul className="space-y-2 space-x-2">
+        <ul className="space-y-2">
           {todos.map((todo) => (
             <li key={todo.id} className="mb-2 flex justify-between">
               <Link
-                href={`todo/${todo.id}`}
-                //   link to todo page
+                href={`/todo/${todo.id}`}
                 className="text-gray-800 hover:underline"
               >
                 {todo.title}
@@ -34,9 +45,8 @@ export default async function page() {
         </ul>
       </div>
 
-      {/* go to todo add page */}
-      <div className="mt-4 ">
-        <Link href="/" className="text-indigo-600 hover:underline">
+      <div className="mt-4">
+        <Link href="/add-todo" className="text-indigo-600 hover:underline">
           Add a new task
         </Link>
       </div>
